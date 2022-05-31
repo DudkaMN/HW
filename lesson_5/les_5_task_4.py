0d4d24af8f5212eae59f7af4e0fa9479471a92c2
DudkaMN <104623767+DudkaMN@users.noreply.github.com>
2022-05-31 20:12:48 +0300

    um task_4

--- lesson_5/les_5_task_4.py
+++ lesson_5/les_5_task_4.py
@@ -3,5 +3,5 @@ from googletrans import Translator
 translator = Translator()
 with open('task_4.txt', 'r', encoding='utf-8') as f:
     for line in f:
-        ft = translator.translate(line, 'ru')
+        ft = (translator.translate(line, 'ru')).text
         print(ft)
